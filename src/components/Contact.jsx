fcd4fd378ce9a3748b9b2a831966af988a301e99
Lisandro H G Mosconi Fariña <lisandro.mosconi@gmail.com>
2026-04-27 19:40:23 +0000

    feat: added Edit button to Contact card

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,14 +1,22 @@
 import contactAPI from "../services/contactAPI"
+import { Link, useNavigate } from "react-router-dom";
 
 const Contact = ({ contact }) => {
+
+    const navigate = useNavigate()
+    const handleNavigate = () => {
+        navigate('/edit')
+    }
+
     return (
-        <div className="card" style={{width: "18rem"}}>
+        <div className="card" style={{ width: "18rem" }}>
             <img src="https://www.zonammorpg.com/wp-content/uploads/2009/07/Armus.jpg" />
             <div className="card-body">
                 <h5>{contact.name}</h5>
                 <p>{contact.phone}</p>
                 <p>{contact.email}</p>
                 <p>{contact.address}</p>
+                <button onClick={handleNavigate}>Edit</button>
             </div>
         </div>
     );
